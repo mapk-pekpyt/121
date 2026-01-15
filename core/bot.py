3e60ca7221cd6e59b0e96da145e2df02f7c90612
mapk-pekpyt <79519532+mapk-pekpyt@users.noreply.github.com>
2026-01-15 18:30:52 +0100

    Update bot.py

--- core/bot.py
+++ core/bot.py
@@ -1,1 +1,7 @@
-#
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
+from config import GROQ_API_KEY
+
+bot = Bot(token=os.getenv("BOT_TOKEN"))
+storage = MemoryStorage()
+dp = Dispatcher(storage=storage)
