b8cfe98e3384e5f516517c4f128e11b569e00832
mapk-pekpyt <79519532+mapk-pekpyt@users.noreply.github.com>
2026-01-17 12:04:48 +0100

    Update __init__.py

--- services/__init__.py
+++ services/__init__.py
@@ -1,1 +1,11 @@
-#
+# Импортируем ключевые сервисы
+from .ai_client import ask_groq
+from .memory import memory
+from .analytics import *
+from .moderator import Moderator
+
+__all__ = [
+    'ask_groq',
+    'memory',
+    'Moderator'
+]
