dab8a750d2a30b8fc6fdd0944b9b2afb0a58880f
mapk-pekpyt <79519532+mapk-pekpyt@users.noreply.github.com>
2026-01-17 12:08:37 +0100

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -4,6 +4,7 @@ from .user import router as user_router
 from .chat_monitor import router as chat_monitor_router
 from .personal import router as personal_router
 from .advertising import router as advertising_router
+from . import admin, user, chat_monitor, personal, advertising
 
 __all__ = [
     'admin_router',
